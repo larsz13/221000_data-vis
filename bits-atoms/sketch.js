1a9f5358f7bcb0a9fa22befa4dd7b6cfd0c167d9
larsz13 <lars.ziegler@zhdk.ch>
2022-10-20 11:09:27 +0200

    Initial commit

---
+++ bits-atoms/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(800, 800);
+    rectMode(CENTER);
+    noStroke();
+    fill(0,0,255)
+    }
+  
+  function draw() {
+    background(0);
+    circle(mouseX,mouseY,200);
+    }
